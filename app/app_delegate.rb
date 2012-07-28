63ade6b81a2829daf353f246be4ce43bc3b34054
Akshay Rawat <projects@akshay.cc>
2012-07-28 14:58:27 +0530

    nav bar colors

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,7 +3,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = UINavigationController.alloc.initWithRootViewController(ProjectsController.alloc.init)
-    @window.rootViewController.navigationBar.tintColor = UIColor.grayColor
+    @window.rootViewController.navigationBar.tintColor = UIColor.colorWithRed(0.227, green:0.302, blue:0.502, alpha:1)
     @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
     true
