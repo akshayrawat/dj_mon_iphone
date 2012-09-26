3e1bed87e77423fb757786838cebb4cbc10b6416
Timo Schilling <timo@schilling.io>
2012-09-26 02:36:24 +0200

    AppDelegate store @project_controller in an instance variable

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,9 @@
 class AppDelegate
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @project_controller = ProjectsController.alloc.init
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(ProjectsController.alloc.init)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(@project_controller)
     @window.rootViewController.navigationBar.tintColor = UIColor.blackColor
     @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
